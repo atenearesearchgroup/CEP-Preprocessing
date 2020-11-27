ca2640f802feb90d7d77ffb120801f408fec37b2
aurorarq <aurorarq89@gmail.com>
2020-11-27 19:41:47 +0100

    Added window-based evaluation for experimentation

--- code/cepdm/src/es/uma/atenea/cepdm/learning/MOAClassificationExperiment.java
+++ code/cepdm/src/es/uma/atenea/cepdm/learning/MOAClassificationExperiment.java
@@ -2,15 +2,19 @@ package es.uma.atenea.cepdm.learning;
 
 import com.yahoo.labs.samoa.instances.Instance;
 
+import es.uco.kdis.datapro.dataset.column.IntegerColumn;
+import es.uco.kdis.datapro.dataset.column.NumericalColumn;
+import es.uco.kdis.datapro.dataset.source.CsvDataset;
 import moa.classifiers.Classifier;
 import moa.core.InstanceExample;
 import moa.core.TimingUtils;
 import moa.evaluation.BasicClassificationPerformanceEvaluator;
+import moa.evaluation.WindowClassificationPerformanceEvaluator;
 
 /** 
  * A wrapper for MOA classification algorithms.
  * @author Aurora
- * @version 1
+ * @version 2
  * */
 
 public abstract class MOAClassificationExperiment extends MOAExperiment{
@@ -27,6 +31,37 @@ public abstract class MOAClassificationExperiment extends MOAExperiment{
 	 * */
 	public MOAClassificationExperiment(String dataset) {
 		super(dataset);
+	}
+	
+	/**
+	 * Parameterized constructor
+	 * @param dataset The file path of the dataset
+	 * @param numExecution The identifier of the current execution
+	 * */
+	public MOAClassificationExperiment(String dataset, int numExecution) {
+		super(dataset, numExecution);
+	}
+	
+	/**
+	 * Parameterized constructor
+	 * @param dataset The file path of the dataset
+	 * @param numExecution The identifier of the current execution
+	 * @param frequency The reporting frequency
+	 * */
+	public MOAClassificationExperiment(String dataset, int numExecution, int frequency) {
+		super(dataset, numExecution, frequency);
+	}
+	
+	/**
+	 * Parameterized constructor
+	 * @param dataset The file path of the dataset
+	 * @param numExecution The identifier of the current execution
+	 * @param frequency The reporting frequency
+	 * @param numberrOfClasses Number of classes for prediction
+	 * */
+	public MOAClassificationExperiment(String dataset, int numExecution, int frequency, int numberOfClasses) {
+		super(dataset, numExecution, frequency);
+		this.numClasses = numberOfClasses;
 	}
 	
 	/**
@@ -48,15 +83,69 @@ public abstract class MOAClassificationExperiment extends MOAExperiment{
 	 * performance measures
 	 * */
 	protected void createEvaluator() {
+		// Standard evaluator with optional statistics set to true
 		this.evaluator = new BasicClassificationPerformanceEvaluator();
-		// Set optional statistics to true
 		((BasicClassificationPerformanceEvaluator)this.evaluator).precisionRecallOutputOption.setValue(true);
 		((BasicClassificationPerformanceEvaluator)this.evaluator).f1PerClassOption.setValue(true);
 		((BasicClassificationPerformanceEvaluator)this.evaluator).recallPerClassOption.setValue(true);
 		((BasicClassificationPerformanceEvaluator)this.evaluator).precisionPerClassOption.setValue(true);
 		((BasicClassificationPerformanceEvaluator)this.evaluator).prepareForUse();
+		
+		// Window-based evaluator with optional statistics set to true
+		this.wEvaluator = new WindowClassificationPerformanceEvaluator();
+		((WindowClassificationPerformanceEvaluator)this.wEvaluator).precisionRecallOutputOption.setValue(true);
+		((WindowClassificationPerformanceEvaluator)this.wEvaluator).f1PerClassOption.setValue(true);
+		((WindowClassificationPerformanceEvaluator)this.wEvaluator).recallPerClassOption.setValue(true);
+		((WindowClassificationPerformanceEvaluator)this.wEvaluator).precisionPerClassOption.setValue(true);
+		((WindowClassificationPerformanceEvaluator)this.wEvaluator).prepareForUse();
 	}
 		
+	/**
+	 * Create the datasets to add measurements during evaluation
+	 * */
+	protected void createResDataset() {
+		
+		this.resDataset = new CsvDataset();
+		this.wResDataset = new CsvDataset();
+		
+		// First column is the number of samples
+		this.resDataset.addColumn(new IntegerColumn("samples"));
+		this.wResDataset.addColumn(new IntegerColumn("samples"));
+		
+		// Each column is a performance measure
+		this.resDataset.addColumn(new NumericalColumn("accuracy"));
+		this.resDataset.addColumn(new NumericalColumn("kappa"));
+		this.resDataset.addColumn(new NumericalColumn("temporalKappa"));
+		this.resDataset.addColumn(new NumericalColumn("mKappa"));
+		this.resDataset.addColumn(new NumericalColumn("f1"));
+		
+		this.wResDataset.addColumn(new NumericalColumn("accuracy"));
+		this.wResDataset.addColumn(new NumericalColumn("kappa"));
+		this.wResDataset.addColumn(new NumericalColumn("temporalKappa"));
+		this.wResDataset.addColumn(new NumericalColumn("mKappa"));
+		this.wResDataset.addColumn(new NumericalColumn("f1"));
+		
+		// Columns for class values
+		for(int i=0; i<this.numClasses; i++) {
+			this.resDataset.addColumn(new NumericalColumn("f1-class"+i));
+			this.wResDataset.addColumn(new NumericalColumn("f1-class"+i));
+		}
+		
+		this.resDataset.addColumn(new NumericalColumn("precision"));
+		this.wResDataset.addColumn(new NumericalColumn("precision"));
+		for(int i=0; i<this.numClasses; i++) {
+			this.resDataset.addColumn(new NumericalColumn("precision-class"+i));
+			this.wResDataset.addColumn(new NumericalColumn("precision-class"+i));
+		}
+		
+		this.resDataset.addColumn(new NumericalColumn("recall"));
+		this.wResDataset.addColumn(new NumericalColumn("recall"));
+		for(int i=0; i<this.numClasses; i++) {
+			this.resDataset.addColumn(new NumericalColumn("recall-class"+i));
+			this.wResDataset.addColumn(new NumericalColumn("recall-class"+i));
+		}
+	}
+	
 	/**
 	 * The learning phase following a test-then-train strategy
 	 * */
@@ -81,12 +170,17 @@ public abstract class MOAClassificationExperiment extends MOAExperiment{
 			
 			// Evaluation
 			this.evaluator.addResult(trainExample, ((Classifier)this.learner).getVotesForInstance(trainInstance));
+			this.wEvaluator.addResult(trainExample, ((Classifier)this.learner).getVotesForInstance(trainInstance));
+			if(numberSamples % this.frequency == 0) {
+				updateMeasureDataset(numberSamples);
+			}
 			
 			// Train
 			((Classifier)this.learner).trainOnInstance(trainInstance);
 		}
 		
 		// Also update statistics at the end
+		updateMeasureDataset(numberSamples);
 		this.accuracy = 100.0*(double)numberSamplesCorrect/(double)numberSamples;
 		this.time = TimingUtils.nanoTimeToSeconds(TimingUtils.getNanoCPUTimeOfCurrentThread()-evaluateStartTime);
 	}
--- code/cepdm/src/es/uma/atenea/cepdm/learning/MOAExperiment.java
+++ code/cepdm/src/es/uma/atenea/cepdm/learning/MOAExperiment.java
@@ -1,8 +1,15 @@
 package es.uma.atenea.cepdm.learning;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.yahoo.labs.samoa.instances.Instance;
 
+import es.uco.kdis.datapro.dataset.source.CsvDataset;
+import moa.MOAObject;
 import moa.core.Example;
+import moa.core.Measurement;
 import moa.evaluation.LearningPerformanceEvaluator;
 import moa.learners.Learner;
 import moa.streams.ArffFileStream;
@@ -12,7 +19,7 @@ import moa.streams.ArffFileStream;
  * Subclasses should define those methods that depend on the particular task (regression or classification).
  * 
  * @author Aurora
- * @version 1
+ * @version 2
  * */
 
 public abstract class MOAExperiment {
@@ -31,7 +38,16 @@ public abstract class MOAExperiment {
 	
 	/** The evaluator */
 	protected LearningPerformanceEvaluator<Example<Instance>> evaluator;
-
+	
+	/** The window-based evaluator */
+	protected LearningPerformanceEvaluator<Example<Instance>> wEvaluator;
+	
+	/** The dataset to store the measurements */
+	protected CsvDataset resDataset;
+	
+	/** The dataset to store the window-based measurements */
+	protected CsvDataset wResDataset;
+	
 	/** Total execution time */
 	protected double time;
 	
@@ -88,9 +104,15 @@ public abstract class MOAExperiment {
 		createStreamFromDataset();
 		createAlgorithm();
 		createEvaluator();
+		createResDataset();
 		
 		// Learning
 		testThenTrain();
+		
+		// Post-processing
+		createExperimentFolder();
+		saveModelResults();
+		savePerformanceMeasures();
 	}
 	
 	/**
@@ -116,8 +138,114 @@ public abstract class MOAExperiment {
 	protected abstract void createEvaluator();
 	
 	/**
+	 * Create the columns of the dataset according
+	 * to the selected evaluator. To be refined
+	 * by subclasses.
+	 * */
+	protected abstract void createResDataset();
+	
+	/**
 	 * The learning method. To be refined by
 	 * subclasses according to the learning task.
 	 * */
 	protected abstract void testThenTrain();
+	
+	/**
+	 * Save the current performance metrics in the result datasets
+	 * @param currentNumberSamples Current number of samples
+	 * */
+	protected void updateMeasureDataset(int currentNumberSamples) {
+		// Standard evaluator
+		Measurement [] measurements = this.evaluator.getPerformanceMeasurements();
+		int numMeasures = measurements.length;
+		this.resDataset.getColumn(0).addValue(currentNumberSamples);
+		for(int i=1; i<numMeasures; i++) { // Skip first value (number of report)
+			this.resDataset.getColumn(i).addValue(measurements[i].getValue());
+		}
+		
+		// Window-based evaluator
+		Measurement [] wMeasurements = this.wEvaluator.getPerformanceMeasurements();
+		numMeasures = wMeasurements.length;
+		this.wResDataset.getColumn(0).addValue(currentNumberSamples);
+		for(int i=1; i<numMeasures; i++) { // Skip first value (number of report)
+			this.wResDataset.getColumn(i).addValue(wMeasurements[i].getValue());
+		}
+	}
+	
+	/**
+	 * Save the information of the model in a TXT file
+	 * */
+	protected void saveModelResults() {
+		
+		// Get the learned model
+		MOAObject model = this.learner.getModel();
+		StringBuilder sb = new StringBuilder();
+		model.getDescription(sb, 5);
+		
+		// Get the evaluation measurement
+		Measurement [] measurements = this.learner.getModelMeasurements();
+		String learnerName = this.learner.getClass().getSimpleName();
+		
+		// Append the results
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("Dataset: " + this.dataset + "\n");
+		buffer.append("Classifier: " + learnerName + "\n");
+		buffer.append("Time: " + this.time + "\n");
+		for(int i=0; i<measurements.length; i++) {
+			buffer.append("Name: " + measurements[i].getName() + " Value: " + measurements[i].getValue() + "\n");
+		}
+		buffer.append("Model: \n" + sb.toString());
+		
+		// Save in file
+		String outfilename = this.dir + "/" + getExperimentName() + "-model-" + numExecution + ".txt";
+		
+		try {
+			FileWriter writer = new FileWriter(outfilename);
+			writer.write(buffer.toString());
+			writer.close();
+		}catch(IOException e) {
+			System.err.println("Unable to write in file. Output to be shown in console...");
+			System.out.println(buffer.toString());
+		}
+	}
+	
+	/**
+	 * Save the performance measures in a CSV file
+	 * */
+	protected void savePerformanceMeasures() {
+		try {
+			
+			// Standard evaluation metrics
+			String filename = this.dir + "/" + getExperimentName() + "-measures-" + this.numExecution + ".csv";
+			this.resDataset.setNumberOfDecimals(6);
+			this.resDataset.writeDataset(filename);
+			
+			// Window-based evaluation metrics
+			filename = this.dir + "/" + getExperimentName() + "-window-measures-" + this.numExecution + ".csv";
+			this.wResDataset.setNumberOfDecimals(6);
+			this.wResDataset.writeDataset(filename);
+		}catch(IOException e) {
+			System.err.println("Unable to write measures in CSV file.");
+		}	
+	}
+	
+	/**
+	 * Return a name for the current experiment
+	 * containing the algorithm name and the dataset
+	 * @return A string with the experiment name
+	 * */
+	protected String getExperimentName() {
+		String expName = this.learner.getClass().getSimpleName() + "-" + this.dataset.substring(dataset.lastIndexOf("/")+1, dataset.indexOf(".arff"));
+		return expName;
+	}
+	
+	/**
+	 * Create the folders to store the results of this experiment. By default the structure
+	 * is a root folder ("results"), then a folder with the name of the algorithm and
+	 * then a folder with the name of the dataset. 
+	 * */
+	protected void createExperimentFolder() {
+		this.dir = "results/" + this.dataset.substring(dataset.lastIndexOf("/")+1, dataset.indexOf(".arff")) + "/" + this.learner.getClass().getSimpleName();
+		(new File(this.dir)).mkdirs();
+	}
 }
